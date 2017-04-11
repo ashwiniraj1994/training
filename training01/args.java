708503d2a0f0d5eb818f05d25959fe81509de8ba
ashwiniraj1994 <ashwinirajasekar9@gmail.com>
2017-04-10 22:10:14 -0500

    Argument

---
+++ training01/args.java
@@ -0,0 +1,10 @@
+package ashwini;
+
+/**
+ * Created by ashwinirajasekar on 4/10/17.
+ */
+public class args {
+    public static void main(String[] args) {
+        System.out.println(args[0] + args[1] + " welcome to " + args[2]);
+    }
+}
